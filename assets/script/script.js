6d1d192a82485a1fe090fce6a0d005e79768f4fa
AlexR0V <mail.ponomarev@yandex.ru>
2020-05-01 22:58:45 +0300

    anomation+

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,7 +8,36 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-next"></button>',
         autoplay: true,
-        autoplaySpeed: 1500,
+        autoplaySpeed: 4000,
         
     });
+    $('.slider-portfolio-class').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        prevArrow: '<button type="button" class="slick-prev1">Web</button>',
+        nextArrow: '<button type="button" class="slick-next1 flash wow"data-wow-duration="1s" data-wow-delay="10s" data-wow-offset="10" data-wow-iteration="50">Design</button>',
+        fade: true,
+        cssEase: 'linear',
+        
+    });
+});
+
+$(function(){
+    $("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+});
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 800) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+
+    
 });
