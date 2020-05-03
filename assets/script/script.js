90974cbbdc27181a712a15f6a2a07af6721eeea4
AlexR0V <mail.ponomarev@yandex.ru>
2020-05-03 03:02:22 +0300

    +

--- assets/script/script.js
+++ assets/script/script.js
@@ -24,14 +24,6 @@ $(document).ready(function(){
     });
 });
 
-$(function(){
-    $("a[href='#header']").click(function(){
-            var _href = $(this).attr("href");
-            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-            return false;
-    });
-});
-
 $(window).scroll(function(){
     if ($(this).scrollTop() > 800) {
         $('.pageup').fadeIn();
@@ -41,6 +33,13 @@ $(window).scroll(function(){
     
 });
 
+$(function(){
+    $("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+});
 
 
 
